60c03dd13d67ed48f770c800756ed3ec8f90495c
shantoTechwoozy <shanto.techwoozy@gmail.com>
2024-05-16 15:17:59 +0600

    feature section updated

--- components/Features/SingleFeature.tsx
+++ components/Features/SingleFeature.tsx
@@ -24,10 +24,17 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
         whileInView="visible"
         transition={{ duration: 0.5 }}
         viewport={{ once: true }}
-        className="animate_top z-40 rounded-lg border border-white bg-white p-7.5 shadow-solid-3 transition-all hover:shadow-solid-4 dark:border-strokedark dark:bg-blacksection dark:hover:bg-hoverdark xl:p-12.5"
+        className="animate_top z-40 rounded-lg border border-white bg-white p-7.5 shadow-solid-3 transition-all hover:shadow-solid-4 dark:border-strokedark dark:bg-blacksection dark:hover:bg-hoverdark"
       >
-        <div className="relative flex h-16 w-16 items-center justify-center rounded-[4px] bg-primary">
-          <Image src={icon} width={36} height={36} alt="title" />
+        <div className="relative w-full h-[200px] rounded-[5px] overflow-hidden">
+          <Image
+            src={icon}
+            layout="responsive"
+            width={240}
+            height={140}
+            alt={title}
+            className="w-full h-auto object-cover"
+          />
         </div>
         <h3 className="mb-5 mt-7.5 text-xl font-semibold text-black dark:text-white xl:text-itemtitle">
           {title}
--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -3,42 +3,42 @@ import { Feature } from "@/types/feature";
 const featuresData: Feature[] = [
   {
     id: 1,
-    icon: "/images/icon/icon-01.svg",
+    icon: "/images/icon/icon-01.jpg",
     title: "Software Development",
     description:
       "Techwoozy excels in building web, mobile, and desktop apps, fortified by cutting-edge cybersecurity solutions. With a focus on precision and innovation, we transform visions into seamless digital experiences. Trust us to propel your business into the future of technology.",
   },
   {
     id: 2,
-    icon: "/images/icon/icon-02.svg",
+    icon: "/images/icon/icon-02.jpg",
     title: "Software Testing",
     description:
       "Experience peace of mind with our rigorous testing methodologies, meticulously designed to ensure flawless performance of your software. Trust in our expertise to deliver solutions that exceed your expectations, needs every time.",
   },
   {
     id: 3,
-    icon: "/images/icon/icon-03.svg",
+    icon: "/images/icon/icon-03.jpg",
     title: "Digital Marketing",
     description:
       "specializes in digital marketing, offering Facebook page optimization, social media advertising, YouTube marketing, Google My Business creation, SMS and email marketing, website development, local content creation, SEO optimization, Google Ads management, and comprehensive analytical reporting.",
   },
   {
     id: 4,
-    icon: "/images/icon/icon-04.svg",
+    icon: "/images/icon/icon-04.jpg",
     title: "Content making & Writing",
     description:
       "we excel in creating engaging blog posts, captivating image and video content, and eye-catching banner designs, tailored to elevate your brand's online presence.",
   },
   {
     id: 5,
-    icon: "/images/icon/icon-05.svg",
+    icon: "/images/icon/icon-05.jpg",
     title: "Graphics & UI/UX Design",
     description:
       "Graphics Design & UI/UX Design services that elevate user experiences with innovative and captivating visuals, setting your brand apart in the digital world.",
   },
   {
     id: 6,
-    icon: "/images/icon/icon-06.svg",
+    icon: "/images/icon/icon-06.jpg",
     title: "IT Training and Placement",
     description:
       "With our comprehensive training program, expert guidance, and industry connections, we'll help you master web and mobile testing. From learning the latest tools to securing job placements abroad, your journey to a fulfilling career starts here.",
